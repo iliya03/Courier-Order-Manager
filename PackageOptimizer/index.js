d39e2b4704fe5bb954b06da4ad6b9e1fd61c2495
iliya03 <iliyafathma@gmail.com>
2025-04-10 23:51:28 +0545

    refactor changes in index.js

--- PackageOptimizer/index.js
+++ PackageOptimizer/index.js
@@ -17,5 +17,5 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api', orderRoutes);
 
-const PORT = 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
+
